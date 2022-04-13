27a556ac1b808f6cfe965cd77c804d03bc96236e
lvming@ccolsn.com <lvming@ccolsn.com>
2022-04-13 18:20:01 +0800

    图片压缩

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var autoprefixer = require('autoprefixer');
 var pxtoviewport = require('@tcstory/postcss-px-to-viewport');
 var del = require('del');
 var cssmin = require('gulp-cssmin');
-// var imagemin = require('gulp-smushit'); //压缩图片
+var imagemin = require('gulp-imagemin'); //压缩图片
 // var cssnext = require('postcss-cssnext');
 // var shortcss = require('postcss-short');
 
@@ -16,11 +16,11 @@ gulp.task('copy', () => {
     return gulp.src('./src/**/*')
         .pipe(gulp.dest('dist'))
 })
-// gulp.task('imagemin', function() {
-//     return gulp.src('./dist/imgs/**/*.{jpg,png}')
-//         .pipe(imagemin())
-//         .pipe(gulp.dest("./dist/imgs"))
-// });
+gulp.task('imagemin', function() {
+    return gulp.src('./dist/imgs/**/*.{jpg,png}')
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/imgs"))
+});
 
 gulp.task('css', function() {
 
@@ -44,4 +44,4 @@ gulp.task('css', function() {
   .pipe(gulp.dest('dist/css'));
 })
 
-gulp.task('default', gulp.series(['clean','copy','css',]));
+gulp.task('default', gulp.series(['clean','copy','css','imagemin']));
